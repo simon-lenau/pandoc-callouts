af4d5dd37106c6e86d0c1ab716cd5ec403c38088
Simon Lenau <lenau@cispa.dem>
2025-08-27 14:47:17 +0200

    Move functions in separate files

--- lua/utils/process_yaml.lua
+++ lua/utils/process_yaml.lua
@@ -1,10 +1,8 @@
-local names = {"yaml_to_table","define_callout_type"}
+local names = {"yaml_to_table", "define_callout_type"}
 local utils = {}
 for _, name in ipairs(names) do
     utils[name] = require("utils." .. name)
 end
-
-
 
 -- ============================ > process_yaml < ============================ --
 
@@ -12,7 +10,7 @@ end
 -- ││ This will define callout types based on the yaml header_counts       ││ --
 -- └└──────────────────────────────────────────────────────────────────────┘┘ --
 
-local function process_yaml(meta,references)
+local function process_yaml(meta, references)
     local callout_types = meta['callout-types']
     if type(callout_types) ~= "table" then
         return meta
